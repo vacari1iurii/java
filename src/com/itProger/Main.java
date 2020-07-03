5447bea03d71e484bfd840d42c8bb792be7eb8bc
Vacari <vacari1iurii@gmail.com>
2020-07-03 19:19:51 +0300

    Adding two java files

---
+++ src/com/itProger/Main.java
@@ -0,0 +1,10 @@
+package com.itProger;
+import java.sql.SQLException;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("asd");
+    }
+}
